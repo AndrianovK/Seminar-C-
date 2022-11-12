10d79cbf125c7d50917f4b54981c753150050fdb
Andrianov Konstantin <publicopinion2@yandex.ru>
2022-11-12 14:52:30 +0300

    Task01

--- Task01/Program.cs
+++ Task01/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу,которая на входе принимает 2 числа и проверяет является ли первое число больше второго.
+
+Console.Clear
+Console.WriteLine("Введите первое число:");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число:");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if (a== b * b)
+{
+Console.WriteLine("Да");
+}
+else
+{
+Console.WriteLine("Нет")
+}
+
