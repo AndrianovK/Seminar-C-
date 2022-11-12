fcc0822638613346e2959d5978cee7a422514472
Andrianov Konstantin <publicopinion2@yandex.ru>
2022-11-12 15:02:29 +0300

    Task01

--- Task01/Program.cs
+++ Task01/Program.cs
@@ -1,17 +1,19 @@
-﻿// Напишите программу,которая на входе принимает 2 числа и проверяет является ли первое число больше второго.
+﻿// Задача 1
+// Напишите программу,которая на входе принимает 2 числа и проверяет является ли первое число больше второго.
 
-Console.Clear
-Console.WriteLine("Введите первое число:");
+Console.WriteLine("Введите первое число: ");
 int a = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите второе число:");
+Console.WriteLine("Введите второе число: ");
 int b = Convert.ToInt32(Console.ReadLine());
 
-if (a== b * b)
+if(a == b * b)
+
 {
+Console.WriteLine("Результат: ");
 Console.WriteLine("Да");
 }
 else
 {
-Console.WriteLine("Нет")
+Console.WriteLine("Нет");
 }
 
