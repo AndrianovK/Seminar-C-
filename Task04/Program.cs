fb1a4694aab8933b725237f24d5caded7729320f
Andrianov Konstantin <publicopinion2@yandex.ru>
2022-11-13 15:02:27 +0300

    Task04 Complet

--- Task04/Program.cs
+++ Task04/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 4
+//  Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+Console.Clear();
+Console.WriteLine("Введите число ");
+int NumberA = Convert.ToInt32(Console.ReadLine());
+int NumberB = 2;
+ if (NumberA % NumberB == 0 )
+ {
+  Console.WriteLine("Чётное");
+ }
+else
+{
+Console.WriteLine("Нечетное");
+}
