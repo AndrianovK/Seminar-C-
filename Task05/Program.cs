f9a409f27885bafa8311f4df64236bc40b458115
Andrianov Konstantin <publicopinion2@yandex.ru>
2022-11-14 15:37:03 +0300

    Task05 Complete

--- Task05/Program.cs
+++ Task05/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 5
+// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+
+        Console.Clear();
+        Console.WriteLine("Введите число");
+        int NumberA = Convert.ToInt32(Console.ReadLine());
+        int NumberB = 2;
+        int count = 0;
+        if (NumberA % NumberB == 0)
+        {
+            while (count<NumberA);
+            {
+            Console.Write(count+"");
+            count++;
+            }
+        }
+
+
+
+    
