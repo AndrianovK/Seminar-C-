d36ed9ddfc6c4b412b441668ded874c16e83c1b9
Andrianov Konstantin <publicopinion2@yandex.ru>
2022-11-27 17:26:47 +0300

    Task28

--- Task28/Program.cs
+++ Task28/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 28: Напишите программу, которая
+// принимает на вход число N и выдаёт
+// произведение чисел от 1 до N.
+// 4 -> 24
+// 5 -> 120
+
+
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int MultDigits(int num)
+{
+int mult = 1;
+for (int i = 1; i <= num; i++)
+{
+mult = mult * i;
+}
+
+return mult;
+}
+
+if (number > 0)
+{
+int multDigits = MultDigits(number);
+Console.WriteLine($"Произведение чисел от 1 до {number} = {multDigits}");
+}
+else System.Console.WriteLine("Требуется ввести натуральное число");
