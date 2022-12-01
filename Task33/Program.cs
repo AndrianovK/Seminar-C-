33ee573e87b27b52f4823523d115572839351168
Andrianov Konstantin <publicopinion2@yandex.ru>
2022-12-01 13:31:55 +0300

    Task33

--- Task33/Program.cs
+++ Task33/Program.cs
@@ -4,7 +4,8 @@
 // 4; массив [6, 7, 19, 345, 3] -> нет
 // 3; массив [6, 7, 19, 345, 3] -> да
 
-
+int[] CreateArrayRndInt (int size, int min, int max)
+{
 int[] arr = new int[size];
 Random rnd = new Random();
 
@@ -15,31 +16,30 @@ arr[i] = rnd.Next(min, max + 1);
 return arr;
 }
 
-void PrintArray(int[] arr)
+void PrintArray (int[] arr)
 {
 Console.Write("[");
 for (int i = 0; i < arr.Length; i++)
 {
-if (i < arr.Length - 1)
-Console.Write($"{arr[i]}, ");
+if(i < arr.Length - 1) Console.Write($"{arr[i]}, ");
 else Console.Write($"{arr[i]}");
 }
 Console.WriteLine("]");
 }
 
-int[] array = CreateArrayRndInt(5, 0, 999);
+int[] array = CreateArrayRndInt(12, -9, 9);
 PrintArray(array);
 
-bool FindNumberArray(int[] arr, int numb)
-{
-for (int i = 0; i < arr.Length; i++)
-{
-if (arr[i] == numb) return true;
-}
-return false;
-}
 
+bool FindNumberArray(int[] arr , int numb)
+{
+    for(int i = 0 ; i < array.Length ; i ++)
+    {
+        if (arr[i]== numb) return true;
+        return false;
+    }
+}
 Console.Write("Введите искомое число: ");
 int number = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Заданное число присутствует в массиве?");
-Console.WriteLine(FindNumberArray(array, number) ? "да" : "нет");
+Console.WriteLine("Заданное число присутсвует в массиве?");
+Console.WriteLine(FindNumberArray(array,number) == true ? "да":"нет" );
